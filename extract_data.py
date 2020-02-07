cb886b47da3fbf5441a313388c1632dcad13296c
Jason Kao <jason.kao@columbia.edu>
2020-02-06 19:12:55 -0500

    extract data script

---
+++ extract_data.py
@@ -0,0 +1,25 @@
+filenames = {
+    20: 'nyc_pluto_20v1_csv/pluto_20v1.csv',
+    17: 'Borofiles_CSV/MN2017V1.csv',
+    14: 'nyc_pluto_14v1/MN.csv',
+    11: 'nyc_pluto_11v1/MN11v1.txt',
+}
+
+def extract_columbia(f):
+    output = [ next(f) ]
+
+    for line in f:
+        if 'trustees of columbia' in line.lower():
+            output.append(line)
+            print(len(output), end='\r')
+    return output
+
+if __name__ == '__main__':
+    for y in filenames.keys():
+        print('Year', y, '=' * 5)
+        with open('data/' + filenames[y]) as f:
+            rows = extract_columbia(f)
+            print(len(rows))
+        with open(f'out-{y}.csv', 'w') as f:
+            f.write(''.join(rows))
+            print('wrote', y)
